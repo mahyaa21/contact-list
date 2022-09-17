afe9b77d74c4461f7e0b7d4f99febca2dea4103f
mahya <mahya@mahyas-MacBook-Pro.local>
2022-09-17 19:47:49 +0430

    feat: add search func to search box

--- frontend/src/components/searchBox/SearchBox.tsx
+++ frontend/src/components/searchBox/SearchBox.tsx
@@ -2,27 +2,27 @@ import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { InputWrapper } from "../widgets";
 import { searchContact } from "../../store/contact/action";
-const _ = require("lodash");
+import { throttle } from "lodash";
 const SearchBox = () => {
 	const [searchContent, setSearchContent] = useState<string>();
 	const dispatch = useDispatch();
-	const search = (e: any) => {
+	const searchContentOnchange = (e: any) => {
 		setSearchContent(e.target.value);
+		search();
 	};
-	useEffect(() => {
-		_.debounce(() => {
-			let searchParams: string;
-			if (Number(searchContent)) {
-				searchParams = `\"phone\":{\"contains\":\"${searchContent}\"}`;
-			} else {
-				searchParams = `\"first_name\":{\"contains\":\"${searchContent}\"}`;
-			}
-			dispatch(searchContact(searchParams) as any);
-		}, 1000);
-	}, [searchContent]);
+	const search = throttle(() => {
+		let searchParams: string;
+		if (Number(searchContent)) {
+			searchParams = `\"phone\":{\"contains\":\"${searchContent}\"}`;
+		} else {
+			searchParams = `\"first_name\":{\"contains\":\"${searchContent}\"}`;
+		}
+		dispatch(searchContact(searchParams) as any);
+	}, 1000);
+
 	return (
 		<div>
-			<InputWrapper value={searchContent} onChange={search} />
+			<InputWrapper value={searchContent} onChange={searchContentOnchange} />
 		</div>
 	);
 };
