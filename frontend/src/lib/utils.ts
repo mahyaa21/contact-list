f72864b178b2fad887a5979d65bd55e766fdb151
mahya <mahya@mahyas-MacBook-Pro.local>
2022-09-20 14:15:56 +0430

    fix: remove normalizeArray func

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -2,23 +2,6 @@ class Utils {
   static get baseUrl(): string {
     return "http://localhost:1337";
   }
-  
-  static normalizeCashedArray = (
-		array: Array<any>,
-		item: any,
-		normalLength: number
-	): Array<any> | null => {
-		if (item && !array?.includes(item) && array?.length < normalLength) {
-			const newRecentContact = [...array, item];
-			return newRecentContact;
-		} else if (item && !array?.includes(item) && array?.length >= normalLength) {
-			array?.pop();
-			array?.unshift(`${item}`);
-			return array;
-		} else {
-			return null;
-		}
-	};
 }
 
 export default Utils;
