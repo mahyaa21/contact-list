65032e82ade86eafd217c24341a3d36724077a10
mahya <mahya@mahyas-MacBook-Pro.local>
2022-09-20 16:26:21 +0430

    fix: change search box data flow

--- frontend/src/components/searchBox/SearchBox.tsx
+++ frontend/src/components/searchBox/SearchBox.tsx
@@ -1,26 +1,17 @@
-import { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useState } from "react";
 import { InputWrapper } from "../widgets";
-import { searchContact } from "../../store/contact/action";
-import { throttle } from "lodash";
 import EditorSearchIcon from '@atlaskit/icon/glyph/editor/search'
 import style from "./SearchBox.module.scss";
-const SearchBox = () => {
+
+interface SearchBoxInterface {
+	onChange: (e: any) => void;
+}
+const SearchBox = ({ onChange }: SearchBoxInterface) => {
 	const [searchContent, setSearchContent] = useState<string>();
-	const dispatch = useDispatch();
 	const searchContentOnchange = (e: any) => {
 		setSearchContent(e.target.value);
-		search();
+		onChange(e.target.value);
 	};
-	const search = throttle(() => {
-		let searchParams: string;
-		if (Number(searchContent)) {
-			searchParams = `\"phone\":{\"contains\":\"${searchContent}\"}`;
-		} else {
-			searchParams = `\"first_name\":{\"contains\":\"${searchContent}\"}`;
-		}
-		dispatch(searchContact(searchParams) as any);
-	}, 1000);
 
 	return (
 		<div className={style.searchBoxContainer}>
