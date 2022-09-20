6face0e37e2ee7e4f34e2a1e5e3f221f297cfa86
mahya <mahya@mahyas-MacBook-Pro.local>
2022-09-20 14:15:02 +0430

    fix: refactor contactList

--- frontend/src/components/contactList/ContactList.tsx
+++ frontend/src/components/contactList/ContactList.tsx
@@ -3,21 +3,24 @@ import { useNavigate } from "react-router-dom";
 import { RootState } from "../../store/rootReducer";
 import { useMemo } from "react";
 import SearchBox from "../searchBox/SearchBox";
-import { getContactById } from "../../store/contact/action";
 import UserAvatarCircleIcon from "@atlaskit/icon/glyph/user-avatar-circle";
 import style from "./ContactList.module.scss";
+import { ContactInterface } from "../../interfaces/contact.interface";
 const ContactList = ({}) => {
 	const navigate = useNavigate();
 	const {
 		list: { items: contacts },
 	} = useSelector(({ contacts }: RootState) => contacts?.data);
+
 	const gotoContactDetail = (id: number) => navigate(`/${id}`);
+
 	const recentContact = useMemo(
 		() => JSON.parse(localStorage.getItem("recent") || "[]"),
 		[localStorage.getItem("recent")]
 	);
-	const renderContactList = useMemo(() => {
-		return contacts.map((item) => (
+
+	function renderContact(array: Array<ContactInterface>) {
+		return array.map((item) => (
 			<div
 				className={style.contact}
 				key={item.id}
@@ -35,29 +38,7 @@ const ContactList = ({}) => {
 				</span>
 			</div>
 		));
-	}, [contacts, contacts.length]);
-	const renderRecentContact = useMemo(() => {
-		return recentContact?.map((item: any) => {
-			return (
-				<div
-					className={style.contact}
-					onClick={() => gotoContactDetail(Number(item.id))}
-				>
-					<div className={style.avatarItem}>
-						{item?.avatar ? (
-							<img className={style.avatar} src={item.avatar} />
-						) : (
-							<UserAvatarCircleIcon label="user" size="xlarge" />
-						)}
-					</div>
-					<span>
-						{item?.first_name} {item?.last_name}
-					</span>
-				</div>
-			);
-		});
-	}, [recentContact]);
-
+	}
 	return (
 		<div className={style.contactListContainer}>
 			<div className={style.contactListWrapper}>
@@ -66,11 +47,11 @@ const ContactList = ({}) => {
 				{recentContact?.length && (
 					<>
 						<div className={style.sectionTitle}>recent</div>
-						<div>{renderRecentContact}</div>
+						<div>{renderContact(recentContact)}</div>
 						<div className={style.separator} />
 					</>
 				)}
-				<div>{renderContactList}</div>
+				<div>{renderContact(contacts)}</div>
 			</div>
 			<div className={style.mainPageImage} />
 		</div>
